8ebd036c31c2a5dfb76c47486bbc03f9c547d9a0
spaceunicorn-jhu <reginamercado.jhu@gmail.com>
2024-10-30 18:59:42 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix())  { # x defaults to empty matrix
+        inv <- NULL #Initialize inverse
+        set <- function(y){
+                x <<- y #Assign mew matrix to x
+                inv <<- NULL #Reset inverse cache
+        }
+        get <- function() {
+                x #Return the matrix
+        
+        }
+        setInverse <- function(inverse){
+                inv <<- inverse #Cache the inverse, stores computed inverse
+        }
+        getInverse <- function(){
+                inv #Return the cache inverse
+        }
+        }
+        
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.  It returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse #Check first if inverse is cached
+        if(!is.null(inv)){
+                message("cached inverse:\n")
+                return(inv) #if already cached, then return the cached inverse
+                }
+        else { #if inv is not already cached, do the calculation
+                data <- x$get() #get the matrix
+                inv <- solve(data, ...) #Compute Inverse of matrix using solve
+                x$setInverse(inv) #Cache the inverse
+                }
+        inv #return the inverse
 }
+
